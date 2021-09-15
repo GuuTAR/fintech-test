042da9f7f2583af46e68f9325892b6d63693da8d
GuuTAR <guitar20122012@gmail.com>
2021-09-16 00:02:33 +0700

    Init single page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
+import CandleChart from './component/Chart/Chart';
+import Header from './component/Header/Header';
+import Loading from './component/loading/Loading';
+import SearchContainer from './component/SearchContainer/SearchContainer';
+import { getPriceBySymbol } from './service/binance.service';
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState([])
+  const [coinName, setCoinName] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const fetchData = async (symbol) => {
+    setLoading(true)
+    const res = await getPriceBySymbol(symbol)
+    setData(res)
+    setCoinName(symbol)
+    setLoading(false)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex-col">
+      <Header />
+      <SearchContainer fetchData={fetchData} />
+      {loading ? <Loading />  : data.length > 0 && <CandleChart data={data} coinName={coinName} />}
     </div>
   );
 }
