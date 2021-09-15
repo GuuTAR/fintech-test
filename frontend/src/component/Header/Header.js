bb82fbbe2e0443cff027eee1be5cbfcbf16b9e2c
GuuTAR <guitar20122012@gmail.com>
2021-09-16 00:03:41 +0700

    Add header

---
+++ frontend/src/component/Header/Header.js
@@ -0,0 +1,15 @@
+import './styles.scss';
+
+const Header = () => {
+  return (
+    <div className="Header flex-row">
+        <h2 className="app-name margin0">CoinToday</h2>
+        <p/>
+        <p/>
+    </div>
+  );
+}
+
+export default Header;
+
+
